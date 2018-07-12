ad7280cdabc820d28234a680e18a4f370962e967
jamma <jeff@jamma.cn>
2018-07-12 08:33:38 +0800

    tests/service.test.js

--- packages/jm-mq-kafka/tests/service.test.js
+++ packages/jm-mq-kafka/tests/service.test.js
@@ -14,3 +14,24 @@ test('send', async () => {
   console.log(doc)
   expect(doc).toBeTruthy()
 })
+
+test('send null', async () => {
+  let doc = await service.send({
+    topic: 'test'
+  })
+  console.log(doc)
+  expect(doc).toBeTruthy()
+})
+
+test('send object', async () => {
+  let doc = await service.send({
+    topic: 'test',
+    message: {
+      id: 123,
+      name: 'jeff'
+    }
+  })
+  console.log(doc)
+  expect(doc).toBeTruthy()
+})
+
